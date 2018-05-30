e0e5127ddbebdca606a8b0c57f73194bef3e8b0f
Ujwala Atre <ujwala.atre@triarqhealth.com>
2018-05-30 12:26:17 +0530

    Initial Commit

---
+++ MyApp01Tests/MyApp01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MyApp01Tests.swift
+//  MyApp01Tests
+//
+//  Created by Mac1 on 30/05/18.
+//  Copyright © 2018 Triarq. All rights reserved.
+//
+
+import XCTest
+@testable import MyApp01
+
+class MyApp01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
